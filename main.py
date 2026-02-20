ad0b4dcc8054fc9419023a105620a0237db0c71f
fertliaa01-hue <fertliaa01@gmail.com>
2026-02-20 14:38:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 import os
-from dotenv import load_dotenv
+from os import getenv
 
-load_dotenv() # Загружает переменные из файла .env в окружение
-
-API_TOKEN = os.getenv('BOT_TOKEN')
-WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+# Бот будет искать эти названия в настройках хостинга
+API_TOKEN = getenv('BOT_TOKEN')
+WEATHER_API_KEY = getenv('WEATHER_API_KEY')
 
 # Проверка, что ключи загрузились
 if not API_TOKEN or not WEATHER_API_KEY:
